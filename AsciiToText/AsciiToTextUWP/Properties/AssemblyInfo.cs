434a9ab3dfe96d575398166bb1c4926f2ab215f9
Valentin Vasilev <brainstew_x_x@outlook.com>
2017-03-07 13:16:49 +0200

    Icon Fixes

--- AsciiToText/AsciiToTextUWP/Properties/AssemblyInfo.cs
+++ AsciiToText/AsciiToTextUWP/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AsciiToTextUWP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ASCII to Text")]
+[assembly: AssemblyDescription("Converts text to ASCII or Unicode and backwards")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AsciiToTextUWP")]
